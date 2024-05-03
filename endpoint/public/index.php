be9ed089cd2fd822f75ea1f484154f7892c6824c
mateg <mategiorbelidze1@gmail.com>
2024-05-03 22:23:39 +0400

    setted up server for cors

--- endpoint/public/index.php
+++ endpoint/public/index.php
@@ -3,6 +3,31 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ ."/../config/DoctrineConfig.php";
 
+function cors() {
+    
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    
+        exit(0);
+    }
+}
+
 // Dotenv setup
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
@@ -16,6 +41,8 @@ $entity_manager = $doctrine_config->getEntityManager();
 if (!isset($_SERVER['REQUEST_METHOD'])) {
     return;
 }
+
+cors();
 
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
     $r->post('/graphql', [App\Controller\GraphQL::class, 'handle']);
--- endpoint/src/Controller/GraphQL.php
+++ endpoint/src/Controller/GraphQL.php
@@ -81,8 +81,13 @@ class GraphQL {
                 ],
             ];
         }
+        header('Access-Control-Allow-Origin: *');
 
+        header('Access-Control-Allow-Methods: GET, POST');
+
+        header("Access-Control-Allow-Headers: X-Requested-With");
         header('Content-Type: application/json; charset=UTF-8');
+
         return json_encode($output);
     }
 }
